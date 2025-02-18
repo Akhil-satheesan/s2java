3d7ee0ad2a48b96715ded60a91c92b5990dc24f8
Akhil-satheesan <akhilsatheesan557@gmail.com>
2025-02-18 02:04:54 -0800

    MatrixAddition

--- MatrixAddition.java
+++ MatrixAddition.java
@@ -13,10 +13,29 @@ public class MatrixAddition {
             {3, 2, 1}
         };
         
-        // Resultant matrix to store the sum
         int[][] result = new int[3][3];
+        System.out.println("First Matrix :");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Second Matrix :");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix2[i][j] + " ");
+            }
+            System.out.println();
+        }
         
-        // Adding matrices
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                result[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        
+        
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 result[i][j] = matrix1[i][j] + matrix2[i][j];
