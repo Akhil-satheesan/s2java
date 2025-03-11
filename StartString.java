87765907aca2dca7b22fc3d9d10c921944c86483
Akhil-satheesan <akhilsatheesan557@gmail.com>
2025-03-11 01:53:54 -0700

    Uppercase and lowercase

---
+++ StartString.java
@@ -0,0 +1,8 @@
+public class StartString {
+    public static void main(String[] args) {
+        String s1="Good Mornig";
+        System.out.println(s1);
+        System.err.println("To lower Case :"+s1.toLowerCase());
+        System.err.println("To Uppercase Case :"+s1.toUpperCase());
+        }
+}
