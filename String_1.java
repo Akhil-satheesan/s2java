94cf0579a7dfd26381e7877dbd851caedb017a6d
Akhil-satheesan <akhilsatheesan557@gmail.com>
2025-03-11 02:12:00 -0700

    Legth substring tetc

---
+++ String_1.java
@@ -0,0 +1,10 @@
+public class String_1 {
+    public static void main(String[] args) {
+        String str="java Programming";
+        System.out.println("The String is"+str);
+        System.out.println("The length of String is"+str.length());
+        System.out.println("Character at second and fourth position is "+str.charAt(1)+","+str.charAt(3));
+        System.out.println("The sub string using start Index only is"+str.substring(5));
+        System.out.println("The sub string using start Index only is"+str.substring(0,4));
+    }
+}
